bdbc99f5c722dfff1e91629ef1ce1a494e62685e
Michele Giugliano <mgiugliano@gmail.com>
2022-10-03 00:03:40 +0200

    Improvement adding webIO pip install

--- pvt/deploy_julia.sh
+++ pvt/deploy_julia.sh
@@ -6,26 +6,39 @@
 
 ! command -v julia --version >/dev/null 2>&1 || { echo "Julia is installed. Nothing to do." >&2; exit 1; }
 
+!pip3 install --upgrade webio_jupyter_extension >/dev/null 2>&1
 
 echo "Installing Julia... please wait (~2min)"
+echo ""
 
 # First, I define the source url (from julialang.org)
 # See: https://julialang.org/downloads/
 url='https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.2-linux-x86_64.tar.gz'
 
 # Next we use the command 'wget' to download and uncompressed the tar archive
-wget -O- $url | tar xz -C /usr/local --strip-components 1
-echo "Julia downloaded and uncompressed!"
-
+echo "Downloading and installing julia from julialang.org..."
+wget -O- $url | tar xz -C /usr/local --strip-components 1 >/dev/null 2>&1
+echo "Done!"
+echo ""
 
 # We finally launch julia non-interactively and instruct it to install a series of packages
-julia -e 'using Pkg; Pkg.add("PackageCompiler"); Pkg.add("PlotlyJS"); Pkg.add("IJulia"); Pkg.precompile("PackageCompiler"); Pkg.precompile("PlotlyJS"); Pkg.precompile("IJulia");'
-
-echo "Julia packages have been installed!"
+echo "Installing packages... (it takes long)"
+julia -e 'using Pkg; Pkg.add("PackageCompiler"); Pkg.add("PlotlyJS"); Pkg.add("IJulia"); Pkg.precompile("PackageCompiler"); Pkg.precompile("PlotlyJS"); Pkg.precompile("IJulia");' >/dev/null 2>&1
+echo "Done!"
+echo ""
 
 # as well as precompile them
-julia -e 'using Pkg; Pkg.precompile("PackageCompiler"); Pkg.precompile("PlotlyJS"); Pkg.precompile("IJulia"); using PackageCompiler; using PlotlyJS; using IJulia; using PlotlyJS; p = plot(rand(2,2)); using IJulia; installkernel("Julia");'
+echo "Precompiling packages and installing kernel... (it takes long)"
+julia -e 'using Pkg; Pkg.precompile("PackageCompiler"); Pkg.precompile("PlotlyJS"); Pkg.precompile("IJulia"); using PackageCompiler; using PlotlyJS; using IJulia; using PlotlyJS; p = plot(rand(2,2)); using IJulia; installkernel("Julia");' >/dev/null 2>&1
+echo "Done!"
+echo ""
 
-echo "Julia packages have been precompiled and IJulia kernel added!"
+# Let's finally make it version-agnostic
+echo "Making julia-version agnostic..."
+mv /root/.local/share/jupyter/kernels/julia-1.8 /root/.local/share/jupyter/kernels/julia >/dev/null 2>&1
+echo "Done!"
+echo ""
 
-echo "You are ready to go! Please refresh the page."
+echo "Congrats!! Installation was successful."
+echo "NOW REFRESH/RELOAD THIS PAGE BY THE BROWSER (CTRL-R OR CMD-R)"
+echo ""
