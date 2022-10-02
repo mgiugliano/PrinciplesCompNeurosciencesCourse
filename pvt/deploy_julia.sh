3e693c26963088628e67d87c0702f42f8d3ca456
Michele Giugliano <mgiugliano@gmail.com>
2022-10-02 20:10:10 +0200

    Deploy script: all in one

--- pvt/deploy_julia.sh
+++ pvt/deploy_julia.sh
@@ -14,7 +14,7 @@ wget -O- $url | tar xz -C /usr/local --strip-components 1
 
 # We finally launch julia non-interactively and instruct it to
 #
-julia -e 'using Pkg; pkg"add IJulia; precompile;"'
-julia -e ./install_packages.jl
-julia -e ./precompile_packages.jl
+julia -e 'using Pkg; Pkg.add("PackageCompiler"); Pkg.add("PlotlyJS"); Pkg.add("IJulia"); Pkg.precompile("PackageCompiler"); Pkg.precompile("PlotlyJS"); Pkg.precompile("IJulia");'
+
+julia -e 'using Pkg; Pkg.precompile("PackageCompiler"); Pkg.precompile("PlotlyJS"); Pkg.precompile("IJulia"); using PackageCompiler; using PlotlyJS; using IJulia; using PlotlyJS; p = plot(rand(2,2)); using IJulia; installkernel("Julia");'
 
