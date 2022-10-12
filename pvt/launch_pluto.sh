fb96ff34e699f630dd789ed734fdec3d775de478
Michele Giugliano <mgiugliano@gmail.com>
2022-10-13 00:21:40 +0200

    Pluto must work

--- pvt/launch_pluto.sh
+++ pvt/launch_pluto.sh
@@ -1,2 +1,2 @@
-julia -O3 --eval 'using Pluto; Pluto.run(Pluto.ServerSession(secret="mg", options=Pluto.Configuration.from_flat_kwargs(host="0.0.0.0", port=1234)))' & 
-npx localtunnel --port 1234
+npx localtunnel --port 1234 & 
+julia -O3 --eval 'using Pluto; Pluto.run(Pluto.ServerSession(secret="mg", options=Pluto.Configuration.from_flat_kwargs(host="0.0.0.0", port=1234)))' 
